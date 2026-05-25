5da273dc21dbf2cc322d9553043d88f2171b9728
Patrick1986Git <patryk.kozlowski@wp.pl>
2026-05-25 18:47:34 +0200

    fix(product): expose insufficient stock details

--- src/main/java/com/company/shop/module/product/exception/ProductInsufficientStockException.java
+++ src/main/java/com/company/shop/module/product/exception/ProductInsufficientStockException.java
@@ -6,6 +6,10 @@ import com.company.shop.common.exception.BusinessException;
 
 public class ProductInsufficientStockException extends BusinessException {
 
+    private final String productName;
+    private final int requestedQuantity;
+    private final int availableQuantity;
+
     public ProductInsufficientStockException(String productName, int requestedQuantity, int availableQuantity) {
         super(HttpStatus.CONFLICT,
                 "PRODUCT_INSUFFICIENT_STOCK",
@@ -13,5 +17,20 @@ public class ProductInsufficientStockException extends BusinessException {
                 new Object[] { productName, requestedQuantity, availableQuantity },
                 "Insufficient stock for product: " + productName + ". Requested: " + requestedQuantity
                         + ", available: " + availableQuantity);
+        this.productName = productName;
+        this.requestedQuantity = requestedQuantity;
+        this.availableQuantity = availableQuantity;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getRequestedQuantity() {
+        return requestedQuantity;
+    }
+
+    public int getAvailableQuantity() {
+        return availableQuantity;
     }
 }
